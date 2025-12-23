690f4d8c7562ca441b1d7b44bf3bfbdbde409f2f
huy.pham <phamhuy09042002@gmail.com>
2025-12-23 16:42:06 -0500

    add checkout page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { Route, Routes } from 'react-router';
 import './App.css';
+
+import { CheckoutPage } from './pages/CheckoutPage';
 import { HomePage } from './pages/HomePage';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="checkout" element={<CheckoutPage />} />
     </Routes>
   );
 }
