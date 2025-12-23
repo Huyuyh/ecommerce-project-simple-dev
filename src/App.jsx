03e123c5165492d45305f812e438b76687caaf82
huy.pham <phamhuy09042002@gmail.com>
2025-12-23 17:36:52 -0500

    add header component

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@ import './App.css';
 
 import { CheckoutPage } from './pages/CheckoutPage';
 import { HomePage } from './pages/HomePage';
+import { OrdersPage } from './pages/OrdersPage';
+import { TrackingPage } from './pages/TrackingPage';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="checkout" element={<CheckoutPage />} />
+      <Route path="orders" element={<OrdersPage />} />
+      <Route path="tracking" element={<TrackingPage />} />
     </Routes>
   );
 }
